d36b91e3cefb7276dddea1b535711b578ebca2f2
Simon Morley <simon@polkaspots.com>
2016-09-01 00:47:17 +0100

    Fixing segfault in scan / survey

--- src/collector.c
+++ src/collector.c
@@ -350,9 +350,10 @@ void run_interface_scan(json_object *jiface_array,
     while(ptr != NULL)
     {
       printf("Scanning on %s %d\n", ptr->ifname, ptr->val);
-      /* if (!in_array(ptr->val, myArray, 2)) { */
-      /*   myArray[alen] = ptr->val; */
-      /*   alen++; */
+      if (!in_array(ptr->val, myArray, 2)) { // only 2 radios
+        myArray[alen] = ptr->val;
+        alen++;
+        debug("SSSS: %s", myArray[alen]);
         /* if(iw->scan("wlan0", buf_s, &len_s)) { */
     /*       for (i = 0, x = 1; i < len_s; i += sizeof(struct iw_scanlist_entry), x++) */
     /*       { */
@@ -362,7 +363,7 @@ void run_interface_scan(json_object *jiface_array,
     /*         json_object_array_add(jscan_array, jscan); */
     /*       } */
         /* } */
-      /* } */
+      }
       ptr = ptr->next;
     }
 
