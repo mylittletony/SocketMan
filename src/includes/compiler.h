a7032ddf591971e9b50f48e179f7ba72fa531195
Michael Beckers <michael.beckers@devolo.de>
2016-09-21 14:34:52 +0200

    added some more optional underscores

--- src/includes/compiler.h
+++ src/includes/compiler.h
@@ -2,7 +2,7 @@
 #define __compiler_h__
 
 #ifdef __GNUC__
-#define UNUSED(__decl__) __decl__ __attribute((unused))
+#define UNUSED(__decl__) __decl__ __attribute__((__unused__))
 #else
 #define UNUSED(__decl__) __decl__
 #endif
