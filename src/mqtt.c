8360262532952f54637862acfc23524936d38013
Simon Morley <simon@polkaspots.com>
2017-01-19 16:41:04 +0000

    merge conflict, sorry everyone

--- src/mqtt.c
+++ src/mqtt.c
@@ -288,20 +288,6 @@ void mqtt_connect() {
   return;
 }
 
-<<<<<<< HEAD
-// Only send the data once per heartbeat cycle
-int should_ping() {
-  /* int sleep = options.sleep * 2; */
-  int sleep = 180;
-  time_t now = time(NULL);
-  int diff = now - last_ping;
-  if (last_ping == 0 || diff >= sleep) {
-    last_ping = time(NULL);
-    return 1;
-  }
-  return 0;
-}
-
 void ping_mqtt()
 {
   debug("Sending ping to Tony!");
