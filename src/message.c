6a6ac8a86bd43f219c7b8451893492c3b4decc51
Simon Morley <simon@polkaspots.com>
2017-01-23 15:07:02 +0000

    Broken MsgID

--- src/message.c
+++ src/message.c
@@ -113,8 +113,8 @@ void process_message(const char *msg, char *cmd, char *id, char *opType, int len
               case json_type_string:
                 if (strcmp(keym, "msg") == 0)
                   strncpy(cmd, json_object_get_string(valm), len);
-                /* if (strcmp(keym, "type") == 0) */
-                /*   strncpy(opType, json_object_get_string(valm), 10); */
+                if (strcmp(keym, "type") == 0)
+                  strncpy(opType, json_object_get_string(valm), 10);
             }
           }
         }
