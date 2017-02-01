21bc4392b77940430c98358f3a4989a574bd251e
Simon Morley <simon@polkaspots.com>
2017-02-01 05:39:08 +0000

    New network checks

--- src/monitor.c
+++ src/monitor.c
@@ -9,6 +9,7 @@
 #include "system.h"
 #include "collector.h"
 #include "platform.h"
+#include "network.h"
 #include "helper.h"
 #include "ping.h"
 
@@ -20,7 +21,7 @@
 
 // The delay between network restarts when offline
 int delay = 5;
-
+int network_restarted = 1;
 time_t went_offline;
 int tried_backup;
 
@@ -28,7 +29,6 @@ void restart_or_reboot();
 void check_dns_and_recover();
 void check_connection();
 void monitor();
-/* void recover_connection(); */
 
 void reset()
 {
@@ -72,47 +72,8 @@ void check_connection(int reason) {
 
   debug("Device went offline at %lld. Reason code %d, attemping recovery.", (long long) went_offline, reason);
 
-  /* restart_or_reboot(); */
+  restart_or_reboot();
 }
-
-/* void restore_original() */
-/* { */
-/*   if ((remove(NETWORK_FILE) == 0) && (rename(NETWORK_ORIGINAL, NETWORK_FILE) == 0)) {; */
-/*     debug("Restoring original network"); */
-/*     return; */
-/*   } */
-/*   if (copy_file(NETWORK_BAK, NETWORK_FILE) != 1) { */
-/*     if (access(NETWORK_FILE, F_OK ) == -1) */
-/*       recover_network(); */
-/*     debug("I HAVE NO NETWORK FILE!"); */
-/*   } */
-/* } */
-
-/* int should_recover() */
-/* { */
-/*   if (file_present(NETWORK_ORIGINAL)) { */
-/*     debug("I AM GOING TO RESTORE THE ORIGINAL FILE!"); */
-/*     restore_original(); */
-/*   } else if (!tried_backup && file_present(NETWORK_BAK)) { */
-/*     tried_backup = 1; */
-/*     return 1; */
-/*   } */
-/*   return 0; */
-/* } */
-
-/* void recover_network_config() */
-/* { */
-/*   if (copy_file(NETWORK_FILE, NETWORK_ORIGINAL)) */
-/*     debug("Backing up the original network file"); */
-/*   copy_file(NETWORK_BAK, NETWORK_FILE); */
-/* } */
-
-/* void attempt_recovery() */
-/* { */
-/*   if (should_recover()) */
-/*     debug("Trying to connect with the backup config."); */
-/*   recover_network_config(); */
-/* } */
 
 int should_reboot()
 {
@@ -131,60 +92,36 @@ int should_restart_network()
   return 0;
 }
 
-/* int network_restart() { */
-/*   if (should_restart_network()) */
-/*     restart_network(); */
-/*   return 0; */
-/* } */
+int network_restart() {
+  if (should_restart_network())
+    restart_network();
+  return 0;
+}
 
 void restart_or_reboot()
 {
-  /* if (should_reboot()) { */
-  /*   if (reboot() != 0) { */
-  /*     debug("Could not restart the device."); */
-  /*     reset(); */
-  /*   } */
-  /*   return; */
-  /* } else { */
-  /*   network_restart(); */
-  /* } */
-  /* if (delay <= MAX_INTERVAL) */
-  /*   delay *= 2; */
+  if (should_reboot()) {
+    if (reboot() != 0) {
+      debug("Could not restart the device.");
+      reset();
+    }
+    return;
+  }
 
-  /* debug("Network restart delay set to %d seconds. Sleeping for %d before connection check.", delay, OFFLINE_INTERVAL); */
+  if (network_restarted == 0) {
+    network_restarted = 1;
+    network_restart();
+  }
+
+  if (delay <= MAX_INTERVAL)
+    delay *= 2;
+
+  debug("Network restart delay set to %d seconds. Sleeping for %d before connection check.", delay, OFFLINE_INTERVAL);
   /* sleep(OFFLINE_INTERVAL); */
   /* monitor(); */
 }
 
-void recover_connection() {
-  debug("Connection recovered!");
-  reset();
-}
-
-void backup_config()
-{
-  // TODO Needs to NOT backup each time
-  if (copy_file(NETWORK_FILE, NETWORK_BAK)) {
-    debug("Backing up %s config to %s ", NETWORK_FILE, NETWORK_BAK);
-    return;
-  }
-  debug("Could not backup the network config!");
-  return;
-}
-
-/* void heartbeat(int offline_reason) */
-/* { */
-/*   // Better logic here - we now run each time, even if offline */
-/*   if (went_offline == 0) */
-/*     debug("Connection check passed, all systems go."); */
-/*   /1* } else { *1/ */
-/*   /1*   recover_connection(); *1/ */
-/*   /1* } *1/ */
-/*   /1* backup_config(); *1/ */
-
-/*   // Will exit fast if the device isn't initialized. Helps fast init. */
-/*   if (options.initialized) { */
-/*     collect_and_send_data(offline_reason); */
-/*     debug("Sleeping for %d seconds.", options.monitor); */
-/*   } */
+/* void recover_connection() { */
+/*   debug("Connection recovered!"); */
+/*   reset(); */
 /* } */
