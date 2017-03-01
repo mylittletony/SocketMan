64e3ceff7cea64ca7bf551e2e3987e696699331f
Simon Morley <simon@polkaspots.com>
2017-03-01 23:51:45 +0000

    Load5 tests

--- src/collector.c
+++ src/collector.c
@@ -588,13 +588,20 @@ void collect_data(int offline_reason)
   json_object *juptime = json_object_new_int(info.uptime);
   json_object_object_add(jattr, "uptime", juptime);
 
+  debug("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx %f", info.load_1);
+  debug("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx %f", info.load_1);
+  debug("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx %f", info.load_1);
+  debug("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx %f", info.load_1);
+  debug("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx %f", info.load_1);
+  debug("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx %f", info.load_1);
+  debug("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx %f", info.load_1);
+  debug("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx %f", info.load_1);
+
   json_object *jload_1 = json_object_new_double(info.load_1);
   json_object_object_add(jattr, "load_1", jload_1);
 
   json_object *jload_5 = json_object_new_double(info.load_5);
   json_object_object_add(jattr, "load_5", jload_5);
-
-  debug("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx %f", info.load_5);
 
   json_object *jload_15 = json_object_new_double(info.load_15);
   json_object_object_add(jattr, "load_15", jload_15);
