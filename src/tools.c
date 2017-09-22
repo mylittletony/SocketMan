628d0108aca3ee1a4ba5c4ae55d0f3110ceac218
Simon Morley <simon@polkaspots.com>
2017-09-22 16:04:08 +0100

    test exit

--- src/tools.c
+++ src/tools.c
@@ -103,7 +103,8 @@ int connection_check()
   // Check IP - return if we do not have an IP - result 0
   struct defaultRoute dr = route();
   if (strlen(dr.ip) == 0) {
-    return result;
+    debug("EEEEEEEEEEEEEEEEEEEEEEEEEEEGS11");
+    /* return result; */
   }
   result = 1;
 
