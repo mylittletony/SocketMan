396ec65471bd84a9322ae0201b5f01d935a3dadd
Simon Morley <simon@polkaspots.com>
2018-05-12 15:39:30 +0100

    Updates

--- src/mqtt.c
+++ src/mqtt.c
@@ -157,7 +157,7 @@ void delivered(struct mosquitto *mosq, char *mid)
   /*   return val; */
   /* } */
 
-  mosquitto_publish(mosq, 0, delivery, strlen(report), report, 1, false);
+  mosquitto_publish(mosq, 0, delivery, strlen(report), report, 1, true);
   /* publish_message(report, delivery); */
 
   debug("Sleeping for 1 second");
@@ -231,9 +231,9 @@ void my_message_callback(struct mosquitto *mosq, UNUSED(void *userdata), const s
 
   fp = popen(cmd, "r");
   if (fp != NULL) {
-  /*   response = 0; */
-  /*   memset(buffer, '\0', sizeof(buffer)); */
-  /*   fread(buffer, sizeof(char), sizeof(char) * sizeof(buffer), fp); */
+    response = 0;
+    memset(buffer, '\0', sizeof(buffer));
+    fread(buffer, sizeof(char), sizeof(char) * sizeof(buffer), fp);
     pclose(fp);
   }
 
@@ -250,7 +250,6 @@ void my_message_callback(struct mosquitto *mosq, UNUSED(void *userdata), const s
     cmd_notify(response, mid, buffer);
     return;
   }
-
 
   // Refactor
   // Else, let's publish back
